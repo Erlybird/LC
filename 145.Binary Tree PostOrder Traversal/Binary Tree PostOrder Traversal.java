a312d8f7d27bac6987475d69ba614f073085150c
sangram vuppala <vuppala.s@northeastern.edu>
2023-07-17 20:11:34 +0530

    0 ms, faster than 100.00% of Java online submissions

---
+++ 145.Binary Tree PostOrder Traversal/Binary Tree PostOrder Traversal.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//Time Complexity = O(N)
+//Space Complexity = O(1)
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(root,result);
+        return result;
+    }
+
+    private void helper(TreeNode root,List<Integer> result ){
+        //base
+        if(root==null)return;
+        helper(root.left,result);
+        helper(root.right,result);
+        result.add(root.val);
+    }
+}
