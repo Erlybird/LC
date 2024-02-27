4e060f952e53a68e111cb129ffd70e37ae50aeaf
Sangram Vuppala <92066602+Erlybird@users.noreply.github.com>
2024-02-27 13:27:55 -0800

    0 ms, faster than 100.00% of Java online submissions for Nth Digit

---
+++ 400. Nth Digit.java
@@ -0,0 +1,14 @@
+//TC: O(logN) base 10
+//SC: O(1)
+class Solution {
+    public int findNthDigit(int n) {
+     n -= 1;
+     int digits = 1, first = 1;
+     while (n / 9 / first / digits >= 1) {
+         n -= 9 * first * digits;
+         digits++;
+         first *= 10;
+     }
+     return (first + n/digits + "").charAt(n%digits) - '0';
+ }
+ }
